bb96d25fa087aa03adb0934435e5410bad9eefbb
Mark A. Hershberger <mah@nichework.com>
2026-08-29 18:56:09 -0400

    permissions

--- config/settings/global/DefaultSettings.php
+++ config/settings/global/DefaultSettings.php
@@ -5,6 +5,7 @@
 //
 // You can edit or delete this file to change the defaults for all wikis.
 // Per-wiki overrides can be placed in config/settings/wikis/<wiki-id>/.
-$wgGroupPermissions['*']['read'] = false;
 $wgGroupPermissions['*']['edit'] = false;
-$wgGroupPermissions['*']['createaccount'] = false;
+$wgGroupPermissions['user']['edit'] = false;
+$wgGroupPermissions['*']['createaccount'] = true;
+$wgGroupPermissions['sysop']['emailauthorizationconfig'] = true;
